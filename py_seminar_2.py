006c5dcd39c4bbd61dbe58e9fb6c8fef3c61527b
tro1_1a <rudziy.trofim@mail.ru>
2022-09-04 11:07:37 +0300

    Изменил задачи 1 и 2. Добавил 3 задачу

--- py_seminar_2.py
+++ py_seminar_2.py
@@ -1,26 +1,34 @@
 # Задача из дз №1
-x = input(str('Введите любое положительное число: '))
-snum = list(x.strip())
-if '.' in snum:
-    index = snum.index('.')
-    snum.pop(index)
-result = list(map(int, snum))
-s = sum(result)
-print(s)
+# x = str(input('Введите любое положительное число: ').replace('.', '0'))
+# # x = input(str('Введите любое положительное число: '))
+# snum = list(x.strip())
+# # if '.' in snum:
+# #     index = snum.index('.')
+# #     snum.pop(index)
+# result = list(map(int, snum))
+# s = sum(result)
+# print(s)
 
 # Задача из дз 2
-n = int(input('Введите любое положительное число: '))
-nlist = []
-if n > 0:
-    i = 0
-    x = 1
-    while i is not n:
-        x = x*(i+1)
-        nlist.append(x)
-        i += 1
-    print(nlist)
-elif n == 0:
-    x = 0
-    print(nlist)
-else:
-    print(str('Вы ввели некорректное значение!'))
+# n = int(input('Введите любое положительное число: '))
+# nlist = []
+# if n > 0:
+#     i = 0
+#     x = 1
+#     while i < n:
+#         x = x*(i+1)
+#         nlist.append(x)
+#         i += 1
+#     print(nlist)
+# elif n == 0:
+#     x = 0
+#     print(nlist)
+# else:
+#     print(str('Вы ввели некорректное значение!'))
+
+# Задача из дз 3
+import random
+y = str(input('Введите любsые значения через пробел: '))
+list = list(y.split())
+random.shuffle(list)
+print(list)
